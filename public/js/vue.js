e1c60326e771b2d1627a8088e6e25a47087d3494
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-01-14 22:56:33 +0100

    update files

--- public/js/vue.js
+++ public/js/vue.js
@@ -23,19 +23,9 @@ methods: {
         this.jobs = data.items;
         console.log(jobs);
     },
- /*   getJobs() {
-       axios.get('https://api.publish.jobs/api/user-business-jobs').then(response =>
-            this.jobs = response.data);
-            //this.showInfos = true;
-            console.log('jobs');
-        },
-        */
     format(num){
     let res = new Intl.NumberFormat('fr-FR', { maximumSignificantDigits: 3 }).format(num);
     return res;
-},
-    getImgUrl(pic) {
-    return "public/img/" + pic;
-},
+}
 }
   }).mount('#app')
